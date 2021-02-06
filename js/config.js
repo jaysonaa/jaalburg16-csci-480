01435bc91dbfb0191a2ebefee300b51dc9593c38
Jay'son <46541210+jaysonaa@users.noreply.github.com>
2021-02-05 19:21:25 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://main.d3iwy3qb55484y.amplifyapp.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://main.d3iwy3qb55484y.amplifyapp.com' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
